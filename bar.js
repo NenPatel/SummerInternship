c4642fae199353c90665377e1cb9588fd9d0df70
Nen Patel <nenspatel2001@gmail.com>
2022-06-29 21:35:58 +0530

    Initial Commit

--- bar.js
+++
@@ -1,8 +0,0 @@
-// onload = functionCall;
-window.addEventListener('load',functionCall)
-function functionCall() {
-
-    var progress = document.getElementById("barr1")
-    progress.style.width = progress.getAttribute("bar")
-    progress.style.opacity = 1
-}
